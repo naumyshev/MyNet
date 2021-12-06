4586b35f3acb9bea07fda082d62e9cb283f1f7a7
Aleksei Naumyshev <naumyshev@yandex.ru>
2021-12-06 14:14:42 +0300

    deleted Kabzda, Simple components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import Kabzda from "./Kabzda";
-import Simple from "./Simple";
+
 
 
 const App = () => {
     return (
         <div className="App">
 
-            <Kabzda />
-            <Simple />
+
 
         </div>
     );
